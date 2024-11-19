200ab7cde98bab4eb51928c3c4a732a1c40b29e7
valentin781 <valentinjuegos20@gmail.com>
2024-11-19 13:25:52 -0300

    valen

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const registerButton = document.getElementById('registerButton');
 
     loginButton.onclick = () => {
-        window.location.href = 'inicio.html'; // Redirige a inicio.html
+        window.location.href = 'InicioMansa.HTML'; // Redirige a inicio.html
     };
 
     registerButton.onclick = () => {
-        window.location.href = 'inicio.html'; // Redirige a inicio.html
+        window.location.href = 'InicioMansa.HTML'; // Redirige a inicio.html
     };
 });
